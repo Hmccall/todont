64836bfabb6df342f0ad5e62a80c992495a82554
Ubuntu <Hmccall@MyVirtualMachine.qf1nf1klttiefmauttfuo451id.jx.internal.cloudapp.net>
2020-02-19 15:55:28 +0000

    some message

--- index.js
+++ index.js
@@ -5,13 +5,36 @@ const path    = require('path');
 // Gives direct access to GET files from the
 // "public" directory (you can name the directory anything)
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // Default route
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    console.log("GET [/]")
+    let data = {
+        query: req.query,
+        body: req.body,
+        params: req.params,
+    };
+    console.log(data);
+    res.send(JSON.stringify(data));
+});
+
+app.post('/html/todont.html', (req, res) => {
+    console.log("POST [/html/todont.html]")
+    let data = {
+        query: req.query,
+        body: req.body,
+        params: req.params,
+    };
+    console.log(data);
+    res.send(JSON.stringify(data));
 });
 
 // Listen on port 80 (Default HTTP port)
 app.listen(80, () => {
     console.log("Listening on port 80.");
+});
+
+app.get("/todont_list", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/html/todont.html'));
 });
